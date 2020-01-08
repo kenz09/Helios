0ec049cf4293c7dadca5f64cc27344a9426ff6c2
Kenzie <kenzieleonaro@gmail.com>
2020-01-08 22:56:16 +0700

    added some comments

--- app/Message.php
+++ app/Message.php
@@ -9,6 +9,11 @@ class Message extends Model
     //
     protected $fillable = ['message'];
 
+    /**
+     * A message belong to a user
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function user(){
         return $this->belongsTo(User::class);
     }
