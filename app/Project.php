4b897a0a587bb45a162dad81d043d91e8a1f0652
Kenzie <kenzieleonaro@gmail.com>
2020-01-08 08:55:48 +0700

    Modified project variables access

--- app/Project.php
+++ app/Project.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     //
+    protected $fillable = [
+        'name','description',
+    ];
+
     /**
      * The relationship to owned tasks
      *
