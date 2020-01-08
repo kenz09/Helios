163428e9536c67164636c7807dd40bf72a88b121
Kenzie <kenzieleonaro@gmail.com>
2020-01-08 23:16:30 +0700

    modified app.js to accomodate chat message and chat form

--- resources/js/app.js
+++ resources/js/app.js
@@ -30,3 +30,34 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 const app = new Vue({
     el: '#app',
 });
+
+Vue.component('chat-messages', require('./components/ChatMessages.vue'));
+Vue.component('chat-form', require('./components/ChatForm.vue'));
+
+const app = new Vue({
+    el: '#app',
+
+    data: {
+        messages: []
+    },
+
+    created() {
+        this.fetchMessages();
+    },
+
+    methods: {
+        fetchMessages() {
+            axios.get('/messages').then(response => {
+                this.messages = response.data;
+            });
+        },
+
+        addMessage(message) {
+            this.messages.push(message);
+
+            axios.post('/messages', message).then(response => {
+              console.log(response.data);
+            });
+        }
+    }
+});
