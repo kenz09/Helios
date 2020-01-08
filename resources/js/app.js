7914cff3797a9cf3f8ed64e53e78b8e15a0e15fc
Kenzie <kenzieleonaro@gmail.com>
2020-01-09 00:47:14 +0700

    trying to fix vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -27,8 +27,8 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('chat-messages', require('./components/ChatMessages.vue'));
-Vue.component('chat-form', require('./components/ChatForm.vue'));
+Vue.component('chat-messages', require('./components/ChatMessages.vue').default);
+Vue.component('chat-form', require('./components/ChatForm.vue').default);
 
 const app = new Vue({
     el: '#app',
