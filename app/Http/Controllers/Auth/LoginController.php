a71923d60b5af1eb01e4e12ed927297c3e9df5ea
Kenzie <kenzieleonaro@gmail.com>
2020-01-09 00:27:05 +0700

    changed redirect to projects

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/tasks';
+    protected $redirectTo = '/projects';
 
     /**
      * Create a new controller instance.
